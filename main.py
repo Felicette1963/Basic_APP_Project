c7691c76d2193ef9c0ad3525f1793e22c94fcd7d
Felicette1963 <xorudlee@gmail.com>
2023-06-16 10:02:27 +0900

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+def streamlit_head():
+    st.title("나의 파이썬 웹 페이지")
+    st.header("수업 8일차에 만들었어요")
+    st.subheader("그래도 잘 만들었죠?")
+    st.write("내가 만든 페이지, 너를 위해 구웠지")
+
+if __name__ == '__main__':
+    streamlit_head();
+
